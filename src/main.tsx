1f533bcf8a9db1cac6963e5e7e4128183f6e5745
balzhinimaev alexandr <alexandrbnimaev@gmail.com>
2026-02-27 12:57:54 +0000

    fix(runtime): auto-recover from stale chunk load errors

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import AuthWrapper from './AuthWrapper'
+import { setupChunkLoadErrorRecovery } from './utils/chunkRecovery'
 
 // Импортируем типы Telegram
 import './types/telegram'
+
+setupChunkLoadErrorRecovery()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
